2ef5ccd68b6bf093157284c9d74256b50fdfdd1d
rivka <rivkabouhnik@gmail.com>
2021-10-25 18:52:07 +0300

    hi

---
+++ cmake-build-debug/CMakeFiles/hananelProject2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hananelProject2.dir/anomaly_detection_util.cpp.o"
+  "CMakeFiles/hananelProject2.dir/main.cpp.o"
+  "hananelProject2"
+  "hananelProject2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hananelProject2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/hananelProject2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for hananelProject2.
+# This may be replaced when dependencies are built.
