fe23c83535a880dc7b26228b4fb54aa3fca1bda9
DESKTOP-35QM6TF\admin <veredk123@gmail.com>
2021-11-10 16:26:33 +0200

    hi

--- TimeSeries.cpp
+++ TimeSeries.cpp
@@ -4,6 +4,68 @@
 
 #include "TimeSeries.h"
 
+TimeSeries::TimeSeries(const char *CSVfile) {
+    ifstream CSV;
+    CSV.open(CSVfile);
+    if (!CSVfile.is_open()) {
+        throw runtime_error("couldn't open this file.");
+    }
+
+    readTheTitel(CSVfile);
+
+    readTheTable(CSVfile);
+
+    CSV.close();
+}
+
+void TimeSeries::readTheTitle(const char *CSVfile) {
+
+    string line; //first line of the table
+    getline(CSVfile, line);
+    list<string> firstLine = seperationOfWords(line);
+    features = firstLine;
+
+}
+
+void TimeSeries::readTheTable(const char *CSVfile)  {
+
+
+}
+
+list<string> seperationOfWords(string line) {
+
+    int comma = 0 , start = 0;
+    string str;
+    list<string> lis;
+
+    while (comma <= line.size()) {
+        comma = line.find(",", start);
+        lis.push_back(line.substr(start, comma - start));
+        start = comma + 1;
+    }
+    return lis;
+
+}
+
+const vector<vector<float>> TimeSeries::getData() const {
+    return data;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 void TimeSeries::readCSVFile(const char *CSVfile) {
     ifstream CSV;
